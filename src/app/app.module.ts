911abaa5e57faa7ba59970376da0d3940023dd4f
Eduardo Hernandez <waryplumblum@gmail.com>
2022-10-10 11:12:44 -0600

    Proyecto terminado hasta 10/10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductFormComponent } from './components/product-form/product-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProductListComponent,
+    ProductFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/interfaces/Product';
+
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts(){
+    this.productService.getProducts()
+    .subscribe(
+      res => {
+        this.products = res;
+      },   //console.log(res),               //{this.products = res;},
+      err => console.log(err)
+    ) 
+  }
+
+  deleteProduct(id: string) {
+    this.productService.deleteProduct(id)
+      .subscribe(
+        res => {
+          //console.log(res);
+          this.getProducts();
+        },
+        err => console.log(err)
+      )
+  }
+
+}
